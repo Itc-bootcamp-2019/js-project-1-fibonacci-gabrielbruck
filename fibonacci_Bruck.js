8f494283a36ebc241e651a0f6bfb52e8d043f60e
Gabriel Bruck <gabrielbruck@gabriels-MacBook-Pro.local>
2020-03-01 19:48:36 +0200

    milestone 3

--- fibonacci_Bruck.js
+++ fibonacci_Bruck.js
@@ -1,2 +1,23 @@
-document.getElementById("x").innerText = "4 ";
-document.getElementById("y").innerText = "3";
+// Milestone 2
+// document.getElementById("x").innerText = "4 ";
+// document.getElementById("y").innerText = "3";
+
+//Milestone 3
+
+function fib(x) {
+  let b = 1;
+  let c = 0;
+
+  for (let i = 1; i < x; i++) {
+    y = b + c;
+    c = b;
+    b = y;
+  }
+  return y;
+}
+x = 8;
+console.log(fib(x));
+y - fib(x);
+
+document.getElementById("x").innerText = [x];
+document.getElementById("y").innerText = [y];
