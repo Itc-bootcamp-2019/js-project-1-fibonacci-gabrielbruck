dc6f508e7ec7767719c0c41d70caf519ebc62e3d
gabrielbruck <61509673+gabrielbruck@users.noreply.github.com>
2020-03-03 16:41:32 +0200

    Add files via upload

--- fibonacci_Bruck.js
+++ fibonacci_Bruck.js
@@ -2,22 +2,39 @@
 // document.getElementById("x").innerText = "4 ";
 // document.getElementById("y").innerText = "3";
 
-//Milestone 3
+// //Milestone 3
 
+// function fib(x) {
+//   let b = 1;
+//   let c = 0;
+
+//   for (let i = 1; i < x; i++) {
+//     result = b + c;
+//     c = b;
+//     b = result;
+//   }
+//   return result;
+// }
+// x = 8;
+// console.log(fib(x));
+// result - fib(x);
+
+// document.getElementById("myNumber").innerText = [x];
+// document.getElementById("y").innerText = [result];
+
+// Milestone 4
 function fib(x) {
   let b = 1;
   let c = 0;
 
   for (let i = 1; i < x; i++) {
-    y = b + c;
+    result = b + c;
     c = b;
-    b = y;
+    b = result;
   }
-  return y;
+  return result;
 }
-x = 8;
-console.log(fib(x));
-y - fib(x);
-
-document.getElementById("x").innerText = [x];
-document.getElementById("y").innerText = [y];
+function myFunction() {
+  var x = document.getElementById("myText").value;
+  document.getElementById("demo").innerHTML = fib(x);
+}
