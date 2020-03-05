6a1e53c9498eeffee5473d6e2d34f11d517d3879
gabe <gabrielbruck1@gmail.com>
2020-03-05 11:36:34 +0200

    Milestone 4.1

--- fibonacci_Bruck.js
+++ fibonacci_Bruck.js
@@ -22,15 +22,26 @@
 // document.getElementById("myNumber").innerText = [x];
 // document.getElementById("y").innerText = [result];
 
-// Milestone 4
-function fib(x) {
-  let b = 1;
-  let c = 0;
+// // Milestone 4
+// function fib(x) {
+//   let b = 1;
+//   let c = 0;
 
-  for (let i = 1; i < x; i++) {
-    result = b + c;
-    c = b;
-    b = result;
-  }
-  return result;
+//   for (let i = 1; i < x; i++) {
+//     result = b + c;
+//     c = b;
+//     b = result;
+//   }
+//   return result;
+// }
+
+// Milestone 4.1
+
+function fib(x) {
+  if (x <= 2) return 1;
+  return fib(x - 1) + fib(x - 2);
+}
+function myFunction() {
+  var x = document.getElementById("myText").value;
+  document.getElementById("demo").innerHTML = fib(x);
 }
