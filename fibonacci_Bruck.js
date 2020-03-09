3bf43f045d451649c28820bb85128464e4aee7eb
gabe <gabrielbruck1@gmail.com>
2020-03-09 11:52:18 +0200

    Milestone6

--- fibonacci_Bruck.js
+++ fibonacci_Bruck.js
@@ -46,16 +46,108 @@
 //   document.getElementById("demo").innerHTML = fib(x);
 // }
 
-// Mileston 5
+// // Mileston 5
+// function goSpinning() {
+//   let spinner = document.getElementById("loader");
+//   spinner.classList.toggle("hidden");
+// }
+// function fibonacci() {
+//   let x = document.getElementById("myText").value;
+//   goSpinning();
 
-function myFunction() {
-  let x = document.getElementById("myText").value;
+//   if (checkData(x) !== "okay") {
+//     return console.log("not a valid number");
+//   }
+//   const url = `http://localhost:5050/fibonacci/${x}`;
+//   fetch(url)
+//     .then(response => {
+//       if (response.status === 400) {
+//         return response.text();
+//       } else {
+//         return response.json();
+//       }
+//     })
+//     .then(function(data) {
+//       let y = data.result;
+//       document.getElementById("demo2").innerHTML = y;
+//       console.log(y);
+//       goSpinning();
+//       loader.style.visibility = "hidden";
+//     });
+// }
 
-  fetch(`http://localhost:5050/fibonacci/${x}`)
-    .then(res => res.json())
-    .then(function(data) {
-      let y = data.result;
-      document.getElementById("demo").innerHTML = y;
-      console.log(y);
-    });
+// function checkData(num) {
+//   let alertData = document.getElementById("alertData");
+//   if (num > 50) {
+//     alertData.innerText = "Can't be larger than 50";
+//     alertData.style.display = "block";
+//     return console.log("error");
+//   } else {
+//     return "okay";
+//   }
+// }
+
+// Milestone 6
+function goSpinning() {
+  let spinner = document.getElementById("loader");
+  spinner.classList.replace("hide", "show");
+  setTimeout(() => {
+    spinner.classList.replace("show", "hide");
+  }, 2000);
 }
+function goHidden() {
+  let hiddenResult = document.getElementById("result");
+  hiddenResult.classList.add("hide");
+}
+function goUnHidden() {
+  let hiddenResult = document.getElementById("result");
+  hiddenResult.classList.remove("hide");
+}
+function goUnFifty() {
+  let hiddenResult1 = document.getElementById("ErrorBox");
+  hiddenResult1.classList.remove("hide");
+}
+function goFifty() {
+  let hiddenResult1 = document.getElementById("ErrorBox");
+  hiddenResult1.classList.add("hide");
+}
+
+function goFetch() {
+  document.getElementById("FortyTwoError").innerText = "";
+  goFifty();
+  goHidden();
+  goSpinning();
+
+  let number = document.getElementById("number").value;
+  if (number > 50) {
+    let ErrorFifty = document.getElementById("ErrorBoxText");
+    ErrorFifty.innerText = "Can't be larger than 50";
+    goUnFifty();
+    goSpinning();
+  } else {
+    const url = `http://localhost:5050/fibonacci/${number}`;
+    console.log(url);
+
+    setTimeout(() => {
+      fetch(url)
+        .then(response => {
+          if (response.status === 400) {
+            return response.text();
+          } else {
+            return response.json();
+          }
+        })
+        .then(data => {
+          if (typeof data === "object") {
+            console.log(data.result);
+            document.getElementById("result").innerText = data.result;
+            goUnHidden();
+          } else {
+            document.getElementById("FortyTwoError").innerText =
+              "Server Error: " + " " + data;
+          }
+        });
+    }, 2000);
+  }
+}
+document.getElementById("isBtn").addEventListener("click", goFetch);
